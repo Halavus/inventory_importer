0d2edc9f9f92e2e4bda5161b0854640b90eb351a
halavus <halavus@gmail.com>
2020-02-23 00:00:39 +0100

    save

--- importer.py
+++ importer.py
@@ -4,42 +4,43 @@ import re
 import csv
 from itertools import zip_longest
 
+
 class Importer:
 
     def __init__(self, data):
-        
-        #self.data for testing purposes
+
+        # self.data for testing purposes
         self.data = data
 
-        matches = ["H2O","HE3","NV1","NV2","[A-Z]+"]
+        matches = ["H2O", "HE3", "NV1", "NV2", "[A-Z]+"]
         startmat = '(?<=<span class="MaterialIcon__ticker___1qLDE5z">)'
         startamount = ('(?<=<div class="MaterialIcon__indicator___2QhPuFO'
                        ' MaterialIcon__type-very-small___kE8NFjh">)')
         end = "(?=</)"
 
         regexcolor = ('background: linear-gradient\(135deg,'
-                ' rgb\(\d+, \d+, \d+\),'
-                ' rgb\(\d+, \d+, \d+\)\); color: rgb\(\d+, \d+, \d+\);'
-                ' font-size: 15.84px')
-        
+                      ' rgb\(\d+, \d+, \d+\),'
+                      ' rgb\(\d+, \d+, \d+\)\); color: rgb\(\d+, \d+, \d+\);'
+                      ' font-size: 15.84px')
+
         def regexmaker(
-                matches=matches, 
-                startmat=startmat, 
-                startamount=startamount, 
-                end="", 
+                matches=matches,
+                startmat=startmat,
+                startamount=startamount,
+                end="",
                 separator="|",
                 regexcolor=regexcolor):
 
-            s=""
+            s = ""
             for i in matches:
-                #creates the regex string with correct syntax for every match
-                s=s+startmat+i+end+separator
+                # creates the regex string with correct syntax for every match
+                s = s+startmat+i+end+separator
 
-            s=regexcolor+separator+s+startamount+"\d+"
+            s = regexcolor+separator+s+startamount+"\d+"
             return s
 
-        self.regex=regexmaker()
-       
+        self.regex = regexmaker()
+
         self.match = re.findall(self.regex, data)
 
         def grouper(n, iterable, fillvalue=None):
