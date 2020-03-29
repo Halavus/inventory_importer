f8f194121ac763d7ac8bfd34dbb0116f9d537d7a
halavus <halavus@gmail.com>
2020-03-30 01:59:44 +0200

    cleanup

---
+++ application/modules/invimporter.py
@@ -0,0 +1,24 @@
+import re
+import csv
+from itertools import zip_longest
+
+from .regex import inventory_importer as regex
+
+
+class Importer:
+
+    def __init__(self, data):
+
+        self.match = re.findall(regex, data)
+
+        def grouper(n, iterable, fillvalue=None):
+            "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+            args = [iter(iterable)] * n
+            return list(zip_longest(fillvalue=fillvalue, *args))
+
+        self.grouped = grouper(3, self.match)
+
+        self.nodata=False
+
+        if self.grouped==[]:
+            self.nodata=True
